4b7c729ea0f63c01ecfe59c5dc50c554d3c1d25b
Ramlal S <info2ramlal@gmail.com>
2020-10-20 23:50:14 +0530

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.listen(4000,() => console.log("Listening at 4000"));
+app.listen(process.env.PORT || 4000,() => console.log("Listening at 4000"));
 const path = require('path');
 const fs = require("fs");
 const PDFDocument = require("pdfkit");
